21d307aef50d289454d93c557ff9460f2d842967
Mika Hamalainen <mika.hamalainen@gmail.com>
2018-11-25 21:36:17 +0200

    Replaced HelloWorld with Selector component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Selector selected=""/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Selector from './components/Selector.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Selector
   }
 }
 </script>
---
+++ src/components/Selector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+    <form>
+      <div class="form-group form-row">
+        <label for="selectedInput" class="col-sm-4 col-form-label text-right">
+          <img alt="Vue logo" src="../assets/logo.png" height="24px">
+          Select protein source
+        </label>
+        <div class="col-sm-8">
+          <input type="text" class="form-control" id="selectedInput">
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'selector',
+  props: {
+    selected: String
+  }
+}
+</script>
+
+<style scoped>
+</style>

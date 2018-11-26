76bf211e0ec2a74600e62dd18784f30722d52a99
Mika Hamalainen <mika.hamalainen@gmail.com>
2018-11-26 20:57:13 +0200

    Added more protein sources

--- src/FoodRepository.js
+++ src/FoodRepository.js
@@ -13,7 +13,11 @@ function makeFood(name, energy, carbs, proteins, fats) {
 class FoodRepository {
     constructor() {
         self.foods = [
-            makeFood('Egg', '143', 0.3, 12.5, 10.3)
+            makeFood('Egg', 143, 0.3, 12.6, 9.2),
+            makeFood('Ground Soy', 334, 20.9, 49.2, 2.4),
+            makeFood('Tofu', 82, 1.6, 8.1, 4.8),
+            makeFood('Ground Fava Bean', 331, 42.2, 28.5, 1.9),
+            makeFood('Pulled Oats', 201, 8.1, 30.0, 4.4),
         ]
     }
     readAll() {
